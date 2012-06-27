dba41d8ab253c63a9cc85f6401db9bcb7338f486
erlendve <xelv@PCXELV01.no.sterianet>
2012-06-27 11:24:10 +0200

    Skeleton with servlet

---
+++ src/main/java/no/steria/swhrs/PersonServlet.java
@@ -0,0 +1,22 @@
+package no.steria.swhrs;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PersonServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -1090477374982937503L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		PrintWriter writer = resp.getWriter();
+		
+		writer.append("<html><body>HHer er servlet</body></html>");
+	}
+}
--- src/test/java/no/steria/swhrs/WebtestFrontpage.java
+++ src/test/java/no/steria/swhrs/WebtestFrontpage.java
@@ -5,7 +5,10 @@ import static org.fest.assertions.Assertions.assertThat;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class WebtestFrontpage {
@@ -16,10 +19,28 @@ public class WebtestFrontpage {
 		server.setHandler(new WebAppContext("src/main/webapp", "/"));
 		server.start();
 		int localPort = server.getConnectors()[0].getLocalPort();
-		WebDriver browser = new HtmlUnitDriver();
-		browser.get("http://localhost:" + localPort + "/");
-		assertThat(browser.getPageSource() ).contains("HelloWorld");
+		WebDriver browser = createBrowser();
+		browser.get("http://localhost:" + localPort + "/personMainpage.html");
+		browser.findElement(By.linkText("Create person")).click();
+		browser.findElement(By.name("Name")).sendKeys("Darth");
+		browser.findElement(By.name("CreatePersonButton")).click();
+		browser.findElement(By.linkText("Find person")).click();
+		assertThat(browser.getPageSource()).contains("<li>Darth</li>");
 		
+		
+	}
+
+	private HtmlUnitDriver createBrowser() {
+		return new HtmlUnitDriver() {
+			@Override
+			public WebElement findElement(By by) {
+				try {
+					return super.findElement(by);
+				} catch (NoSuchElementException e) {
+					throw new NoSuchElementException("Did not find " + by + " in " +getPageSource());
+				}
+			}
+		};
 	}
 	
 }
