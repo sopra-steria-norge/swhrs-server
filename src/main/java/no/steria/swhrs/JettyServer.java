b39e22ea666a1d467b6e55d7dccd2bcb5f2b465b
erlendve <xelv@PCXELV01.no.sterianet>
2012-06-29 16:43:44 +0200

    Changed serverport to 22000

--- src/main/java/no/steria/swhrs/JettyServer.java
+++ src/main/java/no/steria/swhrs/JettyServer.java
@@ -5,10 +5,10 @@ import org.eclipse.jetty.webapp.WebAppContext;
 
 public class JettyServer {
 	public static void main(String[] args) throws Exception {
-		Server server = new Server(8081);
+		Server server = new Server(22000);
 		server.setHandler(new WebAppContext("src/main/webapp", "/"));
 		server.start();
-		System.out.println("Server started");
+		System.out.println("Server started!!");
 		
 	}
 }
