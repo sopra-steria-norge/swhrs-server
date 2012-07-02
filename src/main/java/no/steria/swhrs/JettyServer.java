e0f1d41e94ef91945f4b829838cb1952e4dda36f
erlendve <xelv@steria.com>
2012-07-02 12:46:57 +0200

    Try to fix start to fit heroku

--- src/main/java/no/steria/swhrs/JettyServer.java
+++ src/main/java/no/steria/swhrs/JettyServer.java
@@ -5,10 +5,18 @@ import org.eclipse.jetty.webapp.WebAppContext;
 
 public class JettyServer {
 	public static void main(String[] args) throws Exception {
-		Server server = new Server(22000);
+		int localPort = 8081;
+		String envPort = System.getenv("PORT");
+		if (envPort != null && !envPort.isEmpty()) {
+			localPort = Integer.parseInt(envPort);
+		}
+		
+		Server server = new Server(localPort);
 		server.setHandler(new WebAppContext("src/main/webapp", "/"));
 		server.start();
-		System.out.println("Server started!!");
+		System.out.println("Server started!! - port " + localPort);
+		server.join();
+		System.out.println("Dette gikk galt");
 		
 	}
 }
