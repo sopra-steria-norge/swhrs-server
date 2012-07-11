f90d77a3b789704bce362524c187c299b14d77f6
erlendve <xelv@steria.com>
2012-07-11 15:59:39 +0200

    Fixed getHours method in DAO so it gets a HR based on date

--- src/main/java/no/steria/swhrs/HibernateHourRegDao.java
+++ src/main/java/no/steria/swhrs/HibernateHourRegDao.java
@@ -1,18 +1,23 @@
 package no.steria.swhrs;
 
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import javax.naming.NamingException;
 
 import no.steria.swhrs.Person;
 
 import org.eclipse.jetty.plus.jndi.EnvEntry;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import org.hibernate.context.ThreadLocalSessionContext;
+import org.hibernate.criterion.Conjunction;
+import org.hibernate.criterion.Restrictions;
 import org.hsqldb.jdbc.JDBCDataSource;
 import org.joda.time.LocalDate;
 
@@ -51,10 +56,15 @@ public class HibernateHourRegDao implements HourRegDao{
 		//This actually just gets all registrations in the database, so the parameters are useless atm
 		List<HourRegistrationEntity> entityList = session().createCriteria(HourRegistrationEntity.class).list();
 		
+		//I have not figured out how to make a query using hibernate that searches for the specific date. 
+		//Giving up for now... under I will remove the elements with wrong date from the list
+		
 		//This is very ugly, but it's ok for now since all of this is only for local testing
 		List<HourRegistration> list = new LinkedList<HourRegistration>();
 		for (HourRegistrationEntity hr: entityList) {
-			list.add(hr.createHourRegistrationFromEntity());
+			if(hr.getDate().equals(date)) {
+				list.add(hr.createHourRegistrationFromEntity());
+			}		
 		}
 		return list;
 	}
